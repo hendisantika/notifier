95e1a9ed72ae6218c33b014040de7cbf0e295a4e
hendisantika <hendisantika@yahoo.co.id>
2024-05-01 08:24:18 +0700

    Add Sms Model 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notifier/model/Sms.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.notifier.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : notifier
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/1/24
+ * Time: 08:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sms {
+    private String to;
+    private String content;
+}
