d64859fa20de8bd56c30b817db27d3291689a2ca
hendisantika <hendisantika@yahoo.co.id>
2024-05-01 08:45:23 +0700

    Add generate in NotificationGeneratorServiceImpl 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notifier/service/impl/NotificationGeneratorServiceImpl.java
+++ src/main/java/id/my/hendisantika/notifier/service/impl/NotificationGeneratorServiceImpl.java
@@ -1,11 +1,19 @@
 package id.my.hendisantika.notifier.service.impl;
 
+import id.my.hendisantika.notifier.model.Billing;
+import id.my.hendisantika.notifier.model.Customer;
+import id.my.hendisantika.notifier.model.NotificationStatus;
+import id.my.hendisantika.notifier.model.NotificationType;
 import id.my.hendisantika.notifier.service.BillingService;
 import id.my.hendisantika.notifier.service.CustomerService;
 import id.my.hendisantika.notifier.service.NotificationGeneratorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +33,27 @@ public class NotificationGeneratorServiceImpl implements NotificationGeneratorSe
     private final CustomerService customerService;
     private final BillingService billingService;
     private final NotificationContext notificationContext;
+
+    public void generate() {
+        Instant tomorrow = Instant.now().plus(1, ChronoUnit.DAYS);
+        List<Billing> billings = billingService.findByDueDate(tomorrow);
+
+        billings.forEach((Billing billing) -> {
+            try {
+                Customer customer = customerService.getCustomerById(billing.getCustomerId());
+                for (NotificationType type : NotificationType.values()) {
+                    notificationContext.generate(type, billing, customer);
+                }
+
+                billing.setNotificationStatus(NotificationStatus.GENERATED);
+                billing.setChangedDate(Instant.now());
+                billingService.saveBilling(billing);
+
+            } catch (Exception e) {
+                billing.setNotificationStatus(NotificationStatus.FAILED);
+                billing.setChangedDate(Instant.now());
+                billingService.saveBilling(billing);
+            }
+        });
+    }
 }
