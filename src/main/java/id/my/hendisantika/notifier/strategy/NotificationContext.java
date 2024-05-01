0fe7b53d503b02bab1153606dbe0cfc869463ece
hendisantika <hendisantika@yahoo.co.id>
2024-05-01 08:47:07 +0700

    Add autowired in NotificationContext🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notifier/strategy/NotificationContext.java
+++ src/main/java/id/my/hendisantika/notifier/strategy/NotificationContext.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.notifier.strategy;
 
+import id.my.hendisantika.notifier.model.Billing;
+import id.my.hendisantika.notifier.model.Customer;
 import id.my.hendisantika.notifier.model.NotificationType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,6 +22,21 @@ import java.util.Map;
  */
 @Component
 public class NotificationContext {
+    @Autowired
+    public void setSmsStrategy(SmsStrategy smsStrategy) {
+        strategies.put(NotificationType.SMS, smsStrategy);
+    }
+
+    @Autowired
+    public void setEmailStrategy(EmailStrategy emailStrategy) {
+        strategies.put(NotificationType.EMAIL, emailStrategy);
+    }
 
     private final Map<NotificationType, NotificationStrategy> strategies = new HashMap<>();
+
+    public void generate(NotificationType type, Billing billing, Customer customer) throws Exception {
+
+        strategies.values().stream().filter(strategy -> strategy.match(type)).collect(Collectors.toList()).get(0)
+                .generate(billing, customer);
+    }
 }
