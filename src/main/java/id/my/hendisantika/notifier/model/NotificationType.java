99c4e25eb9f8b421f4419ca63eaeaa8a6bcdaa61
hendisantika <hendisantika@yahoo.co.id>
2024-05-01 08:23:53 +0700

    Add NotificationType Enum 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notifier/model/NotificationType.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.notifier.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : notifier
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/1/24
+ * Time: 08:23
+ * To change this template use File | Settings | File Templates.
+ */
+public enum NotificationType {
+    EMAIL,
+    SMS
+}
