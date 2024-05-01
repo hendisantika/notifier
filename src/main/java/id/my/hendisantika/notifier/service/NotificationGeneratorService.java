9cd2d4cbc13efcd50016aba69328137c5ab5998b
hendisantika <hendisantika@yahoo.co.id>
2024-05-01 08:36:22 +0700

    Add NotificationGeneratorService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notifier/service/NotificationGeneratorService.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.notifier.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : notifier
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/1/24
+ * Time: 08:36
+ * To change this template use File | Settings | File Templates.
+ */
+public interface NotificationGeneratorService {
+
+    void generate();
+}
