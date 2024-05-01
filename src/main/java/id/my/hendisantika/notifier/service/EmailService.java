02c7db4a00f23c661b059122ae9885d65646c37a
hendisantika <hendisantika@yahoo.co.id>
2024-05-01 08:35:27 +0700

    Add EmailService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notifier/service/EmailService.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.notifier.service;
+
+import id.my.hendisantika.notifier.model.Email;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : notifier
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/1/24
+ * Time: 08:35
+ * To change this template use File | Settings | File Templates.
+ */
+public interface EmailService {
+
+    void send(Email email);
+}
