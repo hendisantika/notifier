21966f8f4c5258896464ef933c6e5f5debcc49b7
hendisantika <hendisantika@yahoo.co.id>
2024-05-01 08:32:20 +0700

    Add Task 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notifier/task/Task.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.notifier.task;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : notifier
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/1/24
+ * Time: 08:32
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Task {
+
+    void execute();
+}
