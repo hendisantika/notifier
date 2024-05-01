2c3617237a4d1f26159c1f0570a12d0eafb30fc8
hendisantika <hendisantika@yahoo.co.id>
2024-05-01 16:47:38 +0700

    Add remove API in CustomerController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/notifier/controller/CustomerController.java
+++ src/main/java/id/my/hendisantika/notifier/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.notifier.controller;
 
+import id.my.hendisantika.notifier.exception.ConflictException;
 import id.my.hendisantika.notifier.exception.DuplicateException;
 import id.my.hendisantika.notifier.exception.NotFoundException;
 import id.my.hendisantika.notifier.model.Customer;
@@ -10,9 +11,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -69,7 +71,7 @@ public class CustomerController {
     @GetMapping(value = "/{id}")
     public ResponseEntity findById(@Param(value = "Id of customer") @PathVariable("id") Long id) {
         try {
-            Optional<Customer> result = customerService.getCustomerById(id);
+            Customer result = customerService.getCustomerById(id);
             return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (NotFoundException nfe) {
             log.warn("Customer not found!", nfe);
@@ -142,6 +144,29 @@ public class CustomerController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
         } catch (Exception ex) {
             log.warn("Problem in updating Customer!", ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Delete a customer by ID", description = "remove a customer by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Customer deleted."),
+            @ApiResponse(responseCode = "404", description = "Customer with specified id does not exist in the system."),
+            @ApiResponse(responseCode = "409", description = "Customer could not be removed, it's used in system!"),
+            @ApiResponse(responseCode = "500", description = "Server Internal Error")})
+    public ResponseEntity remove(@Param(value = "Id of customer") @PathVariable Long id) {
+        try {
+            customerService.deleteCustomer(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Customer deleted successfully");
+        } catch (EmptyResultDataAccessException nfe) {
+            log.warn("Customer not found!", nfe);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (ConflictException ce) {
+            log.warn("Customer could not be removed, it's used in system!", ce);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        } catch (Exception ex) {
+            log.warn("Problem in removing Customer!", ex);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
