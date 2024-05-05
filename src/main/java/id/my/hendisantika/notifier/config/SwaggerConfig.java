bb618664a859bd7d808fe6f006cda73399516986
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 09:15:24 +0700

    Add Spring Doc 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/notifier/config/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package id.my.hendisantika.notifier.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : notifier
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/5/24
+ * Time: 08:55
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("notification-service")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
+            "-version}") String appVersion) {
+        Contact contact = new Contact();
+        contact.setEmail("hendisantika@yahoo.co.id");
+        contact.setName("HENDI SANTIKA");
+        contact.setUrl("https://www.s.id/hendisantika");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Notification Service Application API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                        .contact(contact)
+                );
+
+    }
+
+}
